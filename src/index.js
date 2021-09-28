c4798d4f93f3bf08eff3605ab89a86cfe0c3952a
Akihiko Shirai <akihiko.shirai@gree.net>
2021-09-29 02:11:26 +0900

    markdownを消す

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ require('reveal.js/dist/theme/black.css')
 let deck = new Reveal({
     slideNumber: 'c/t',
     progress: true,
-    plugins: [Markdown],
+//    plugins: [Markdown],
     plugins: [RevealEmbedTweet]
 })
 deck.initialize();
