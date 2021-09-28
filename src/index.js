325971b4e80dce2e3017bef6ebbd005f63ba1d28
Akihiko Shirai <akihiko.shirai@gree.net>
2021-09-29 01:05:29 +0900

    reveal.js-plugins/embed-tweet/plugin.js'

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 require('reveal.js/dist/reveal.css')
 require('reveal.js/dist/theme/black.css')
+require('reveal.js-plugins/embed-tweet/plugin.js')
 let deck = new Reveal({
     slideNumber: 'c/t',
     progress: true,
-    plugins: [ Markdown ]
+    plugins: [Markdown],
+    dependencies: [
+    { src: 'reveal.js-plugins/embed-tweet/plugin.js' }
+    ]
 })
 deck.initialize();
